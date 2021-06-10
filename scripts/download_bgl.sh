1431bc125df06e65ee33a0e88fc73b8feabdf725
Van Hoang Le <c3363046@uon.edu.au>
2021-06-10 20:31:27 +1000

    update

--- scripts/download_bgl.sh
+++ scripts/download_bgl.sh
@@ -10,5 +10,5 @@ fi
 
 cd $file
 zipfile=BGL.tar.gz?download=1
-wget https://zenodo.org/record/3227177/files/${zipfile} -P $file
+wget https://zenodo.org/record/3227177/files/${zipfile}
 tar -xvzf $zipfile
